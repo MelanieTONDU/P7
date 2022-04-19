e783dd0e79664a092680f6559247b37b9dd7fe4c
Melanie TONDU <melanietondu.2@gmail.com>
2022-04-19 15:05:35 +0200

    test

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,3 +4,11 @@ import router from './router'
 import store from './store'
 
 createApp(App).use(router).use(store).mount('#app')
+
+// I don't use it by default.
+new Vue({
+    router,
+    store,
+    vuetify,
+    render: h => h(App)
+  }).$mount("#app");
